6048596220a604ec11bda9975c9c5ce91c24fc36
xxylu <80449534+xxylu@users.noreply.github.com>
2024-05-10 08:49:30 +0200

    s

---
+++ src/CustomList.java
@@ -0,0 +1,14 @@
+import java.util.AbstractList;
+
+public class CustomList<T> extends AbstractList<T> {
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,14 @@ public class Main {
     public static void main(String[] args) {
         List<Person> people = Person.fromCsv("family.csv");
         System.out.println(people.get(3).generateUML());
-        //for(Person person : people){
-        //    System.out.println(person);
-        //}
+        for(Person person : people){
+            System.out.println(person);
+        }
+
         PlanUMLRunner.setPath("plantuml-1.2024.4.jar");
         String uml = people.get(3).generateUML();
         PlanUMLRunner.generateDiagram(uml,"./", "out2");
-
+        PlanUMLRunner.generateDiagram(uml,"./", "rest");
 
 
     }
